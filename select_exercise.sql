635a2baa9c600085c5df0b2fd98619fa5e273247
Matthew Reat <matthew.reat@gmail.com>
2015-07-28 17:09:04 -0500

    Adding Nirvana portion of exercise

--- select_exercise.sql
+++ select_exercise.sql
@@ -6,7 +6,9 @@ SELECT name AS 'The name of all albums by Pink Floyd' FROM albums WHERE artist =
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released
 SELECT release_date AS 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 -- The genre for Nevermind
+SELECT genre AS 'The genre for Nevermind' FROM albums WHERE name = 'Nevermind';
 -- Which albums were released in the 1990s
+-- SELECT name AS 'Which albums were released in the 1990s' FROM albums WHERE release_date BETWEEN 1989 AND 2000;
 -- Which albums had less than 20 million certified sales
 -- All the albums in the rock genre. Is this all the rock albums in the table?
 
