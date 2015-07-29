8330e20479b7a116b39eaee15b84dd16b415e5c1
Matthew Reat <matthew.reat@gmail.com>
2015-07-29 14:42:49 -0500

    adding updated order by exercise queries

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -3,12 +3,14 @@ FROM employees
 WHERE (first_name LIKE 'Irena' 
 OR first_name LIKE 'Vidya' 
 OR first_name LIKE 'Maya')
-AND gender = 'M';
+AND gender = 'M'
+ORDER BY last_name ASC, first_name DESC;
 
 SELECT concat(first_name, ' ', last_name) AS full_name
 FROM employees
 WHERE last_name LIKE 'E%'
-OR last_name LIKE '%e';
+OR last_name LIKE '%e'
+ORDER BY emp_no DESC;
 
 SELECT concat(first_name, ' ', last_name) AS full_name
 FROM employees
@@ -18,12 +20,13 @@ AND last_name LIKE '%e';
 SELECT concat(first_name, ' ', last_name) AS full_name
 FROM employees
 -- WHERE hire_date LIKE '199%';
-where hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%12-25';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%12-25'
+ORDER BY birth_date ASC, hire_date DESC;
 
 SELECT concat(first_name, ' ', last_name) AS full_name
 FROM employees
-where birth_date LIKE '%12-25';
+WHERE birth_date LIKE '%12-25';
 
 SELECT concat(first_name, ' ', last_name) AS full_name
 FROM employees
